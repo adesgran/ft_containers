258c553a3f64b75b8c3eb110132818722d9ab606
adesgran <adesgran.dev@gmail.com>
2022-09-18 16:00:44 +0200

    Need to fix pair::get

--- containers/utils/pair.hpp
+++ containers/utils/pair.hpp
@@ -6,7 +6,7 @@
 /*   By: adesgran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/17 18:35:47 by adesgran          #+#    #+#             */
-/*   Updated: 2022/09/17 19:40:31 by adesgran         ###   ########.fr       */
+/*   Updated: 2022/09/18 15:53:16 by adesgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,14 @@ namespace ft
 	{
 		typedef	T1	first_type;
 		typedef	T2	second_type;
-		T1 first;
-		T2 second;
+		first_type	first;
+		second_type	second;
 
-		pair() : first(T1()), second(T2()){};
+		pair() : first(), second(){};
 		template<class U, class V> pair(const pair<U, V> &pr) : first(pr), second(pr){};
 		pair(const first_type& a, const second_type &b) : first(a), second(b){};
+		pair (std::piecewise_construct_t pwc, std::tuple<T1> first_args,
+                                   std::tuple<T2> second_args) : first(first_args), second(second_args) {} ;
 
 		template <class T> void swap(T &a, T &b)
 		{
@@ -69,17 +71,19 @@ namespace ft
 
 	template <class T1, class T2>
 		bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
-		{ return !(lhs<rhs); }
+		{ return !(lhs<rhs); };
 
 	template <size_t I, class T1, class T2>
-		typename std::tuple_element<I,pair<T1,T2> >::type&  get (pair<T1,T2>&  pr) noexcept
+		typename std::tuple_element<I,ft::pair<T1,T2>>::type &  get(ft::pair<T1,T2>&  pr) noexcept;
+	/*
 		{
 			if (I == 0)
 				return (pr.first);
 			else if (I == 1)
 				return (pr.second);
 		};
-
+		*/
+/*
 	template <size_t I, class T1, class T2>
 		typename std::tuple_element<I,pair<T1,T2> >::type&&  get (pair<T1,T2>&&  pr) noexcept
 		{
@@ -99,6 +103,7 @@ namespace ft
 				return (pr.second);
 		};
 
+		*/
 };
 
 #endif
